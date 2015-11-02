18dc33fc27fcc1f78bb7218eb970b0c5f6bc8669
banditlev <sditlev@hotmail.com>
2015-11-02 15:27:43 +0100

    Implementation of city added

--- src/hotciv/standard/CityImple.java
+++ src/hotciv/standard/CityImple.java
@@ -1,7 +1,36 @@
 package hotciv.standard;
 
+import hotciv.framework.City;
+import hotciv.framework.Player;
+
 /**
  * Created by sditlev on 02/11/15.
  */
-public class CityImple {
+public class CityImple implements City{
+
+    public Player owner;
+
+    public CityImple(Player _owner) {
+        this.owner = _owner;
+    }
+
+    @Override
+    public Player getOwner() {
+        return owner;
+    }
+
+    @Override
+    public int getSize() {
+        return 0;
+    }
+
+    @Override
+    public String getProduction() {
+        return null;
+    }
+
+    @Override
+    public String getWorkforceFocus() {
+        return null;
+    }
 }
