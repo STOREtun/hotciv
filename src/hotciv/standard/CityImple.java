89697f6b81ba3e86af37717bdef0f1ba4e087784
banditlev <sditlev@hotmail.com>
2015-11-02 15:27:13 +0100

    Initial Tests Complete

---
+++ src/hotciv/standard/CityImple.java
@@ -0,0 +1,7 @@
+package hotciv.standard;
+
+/**
+ * Created by sditlev on 02/11/15.
+ */
+public class CityImple {
+}
--- src/hotciv/standard/GameImpl.java
+++ src/hotciv/standard/GameImpl.java
@@ -30,17 +30,19 @@ import hotciv.framework.*;
 */
 
 public class GameImpl implements Game {
-  public Tile getTileAt( Position p ) { return null; }
-  public Unit getUnitAt( Position p ) { return null; }
-  public City getCityAt( Position p ) { return null; }
-  public Player getPlayerInTurn() { return null; }
-  public Player getWinner() { return null; }
-  public int getAge() { return 0; }
-  public boolean moveUnit( Position from, Position to ) {
+    //Create city instace
+    public CityImple city1 = new CityImple(Player.RED);
+    public Tile getTileAt( Position p ) { return null; }
+    public Unit getUnitAt( Position p ) { return null; }
+    public City getCityAt( Position p ) { return city1; }
+    public Player getPlayerInTurn() { return null; }
+    public Player getWinner() { return null; }
+    public int getAge() { return 0; }
+    public boolean moveUnit( Position from, Position to ) {
     return false;
-  }
-  public void endOfTurn() {}
-  public void changeWorkForceFocusInCityAt( Position p, String balance ) {}
-  public void changeProductionInCityAt( Position p, String unitType ) {}
-  public void performUnitActionAt( Position p ) {}
+    }
+    public void endOfTurn() {}
+    public void changeWorkForceFocusInCityAt( Position p, String balance ) {}
+    public void changeProductionInCityAt( Position p, String unitType ) {}
+    public void performUnitActionAt( Position p ) {}
 }
