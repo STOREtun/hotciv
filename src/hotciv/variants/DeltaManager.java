5ada8e1012cce1023f359a504077f87bf26aa6bf
banditlev <sditlev@hotmail.com>
2015-11-23 18:04:48 +0100

    More stuff i forgot on last push

--- src/hotciv/variants/DeltaManager.java
+++ src/hotciv/variants/DeltaManager.java
@@ -1,7 +1,28 @@
 package hotciv.variants;
 
+import hotciv.framework.*;
+
 /**
  * Created by sditlev on 23/11/15.
  */
-public class DeltaManager {
+public class DeltaManager implements Manager {
+    @Override
+    public WinnerStrategy getWinnerStrategy() {
+        return new AlphaWinnerStrategy();
+    }
+
+    @Override
+    public WorldAgingStrategy getWorldAgingStrategy() {
+        return new AlphaAgingStrategy();
+    }
+
+    @Override
+    public WorldMapStrategy getWorldMapStrategy() {
+        return new DeltaWorldStrategy();
+    }
+
+    @Override
+    public UnitActionStrategy getUnitActionStrategy() {
+        return new AlphaUnitActionStrategy();
+    }
 }
--- test/hotciv/DeltaCiv/TestDeltaCivWorldMap.java
+++ test/hotciv/DeltaCiv/TestDeltaCivWorldMap.java
@@ -1,7 +1,25 @@
 package hotciv.DeltaCiv;
 
+import hotciv.framework.Game;
+import hotciv.framework.Manager;
+import hotciv.standard.GameImpl;
+import hotciv.variants.BetaManager;
+import hotciv.variants.DeltaManager;
+import org.junit.Before;
+
 /**
  * Created by sditlev on 23/11/15.
  */
 public class TestDeltaCivWorldMap {
+
+    private Game game;
+    private Manager manager;
+
+    @Before
+    public void setupGame(){
+        manager = new DeltaManager();
+        game = new GameImpl(manager);
+    }
+
+    
 }
