ea08b923fa171f1b6320822a5b0f57d69a17bddf
banditlev <sditlev@hotmail.com>
2015-12-14 09:36:56 +0100

    fractal map implementation

--- src/hotciv/variants/FractalMap/FractalMapStrategy.java
+++ src/hotciv/variants/FractalMap/FractalMapStrategy.java
@@ -1,7 +1,38 @@
 package hotciv.variants.FractalMap;
 
+import hotciv.framework.Position;
+import hotciv.framework.WorldMapStrategy;
+import hotciv.standard.TileImpl;
+
+import java.util.Map;
+
+import hotciv.standard.WorldMapGenerator;
+import thirdparty.ThirdPartyFractalGenerator;
+
 /**
  * Created by sditlev on 13/12/15.
  */
-public class FractalMapStrategy {
+public class FractalMapStrategy implements WorldMapStrategy {
+    ThirdPartyFractalGenerator fractalGenerator = new ThirdPartyFractalGenerator();
+    WorldMapGenerator mapGenrator = new WorldMapGenerator();
+
+    public String[] fractalStringMapCall(){
+        String line;
+        String[] layout =  new String[]{};
+
+        //Generate string array
+        for ( int r = 0; r < 16; r++ ) {
+            line = "";
+            for ( int c = 0; c < 16; c++ ) {
+                line = line + fractalGenerator.getLandscapeAt(r,c);
+            }
+            layout[r] = line;
+        }
+        return layout;
+    }
+
+    @Override
+    public Map<Position, TileImpl> getWorldLayout() {
+        return mapGenrator.defineWorld(fractalStringMapCall());
+    }
 }
